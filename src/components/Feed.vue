cdeaeb439075c8b8fb6978f3c4cf51ebd95c0e51
Zach Lang <zach.d.lang@gmail.com>
2019-08-19 00:47:04 +1200

    Switching feed to Bulma Message component

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,16 +1,12 @@
 <template>
   <section class="section">
-    <div class="list is-hoverable">
-      <div class="list-item" v-for="article in articles" v-bind:key="article.id">
-        <span>{{ article.name }}</span>
-        <span>{{ article.published }}</span>
-        <a :href="article.url" target="_blank">
-          <span class="icon is-small is-right">
-            <i class="fas fa-external-link-alt"></i>
-          </span>
-        </a>
+    <article class="message" v-for="article in articles" :key="article.id">
+      <div class="message-header">
+        <p>{{ article.name }} {{ article.published }}</p>
+        <button class="delete" aria-label="delete"></button>
       </div>
-    </div>
+      <div class="message-body" v-html="article.content"></div>
+    </article>
   </section>
 </template>
 
